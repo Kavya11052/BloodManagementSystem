095644117cb6b56bcf7b0136b0b7835d0bb07375
Kavya11052 <kavyasaravanan720@gmail.com>
2025-06-30 12:19:30 +0530

    Update UserController.java

--- UserController.java
+++ UserController.java
@@ -1,4 +1,4 @@
-package com.example.demo.controller;
+package com.example.BloodBank.controller;
 
 import java.util.Optional;
 
@@ -14,8 +14,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.example.demo.model.User;
-import com.example.demo.repository.UserRepository;
+import com.example.BloodBank.model.User;
+import com.example.BloodBank.repository.UserRepository;
 
 @RestController
 @RequestMapping("/api")
