d5d0d52e946a3ebeddb1764b6d99e60e3cee353f
Kavya11052 <kavyasaravanan720@gmail.com>
2025-06-30 12:20:09 +0530

    Update UserRepository.java

--- UserRepository.java
+++ UserRepository.java
@@ -1,8 +1,8 @@
-package com.example.demo.repository;
+package com.example.BloodBank.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import com.example.demo.model.User;
+import com.example.BloodBank.model.User;
 
 public interface UserRepository extends JpaRepository<User, Long> {
     // User findByUsername(String username);
